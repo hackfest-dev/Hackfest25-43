1441027f01f2fb6ae3e22b4eeb8e7dccc0d0de5e
Prianshu Sharma <prianshusharma993@gmail.com>
2025-04-19 00:15:01 +0530

    MongoDb connected

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,13 @@
-import mongoose from "mongoose";
+// config/db.js
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(`MongoDB Connection Error: ${error.message}`);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB connected successfully');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+  }
 };
 
-export default connectDB;
+module.exports = connectDB;
